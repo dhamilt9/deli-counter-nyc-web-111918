a9c8ea8eab98687758a9ffb62b9b1b53e5ad54bb
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 19:58:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,9 @@
 def line(array)
   if array.size==0
     puts "The line is currently empty."
+  else
+    returnstring="The line is currently: "
+    array.each_with_index do |item, index|
+      returnstring=returnstring+"#{index}. #{item} "
   end
 end
