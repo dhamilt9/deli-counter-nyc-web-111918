99ecb82a809c52e813240dfda0b3ad427947ba8b
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 20:04:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,4 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  if array.size==0:
-    puts "There is nobody waiting to be served!"
-  else
-    name=array.pop
-    puts "Currently serving #{name}"
-  end
 end
